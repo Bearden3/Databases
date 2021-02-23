a135f5288e9c7043f38fe9b59170c2179f963bb5
Mason <Mason@DESKTOP-NHDIDLB>
2021-02-23 02:39:36 -0600

    Add project files.

---
+++ Database WPF/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Database_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window 
+    {
+        OleDbConnection cn;
+        public MainWindow()
+        {
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\Database1.accdb");
+            InitializeComponent();
+        }
+        
+        private void Assets_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += read[0].ToString() + "                    " + read[1].ToString() + "            " + read[2].ToString() + "\n";
+                
+            }
+
+
+            texty.Text = "Employee Id         Item Id             Description \n" + data;
+            cn.Close();
+        }
+
+        private void Employ_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Employees";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += read[0].ToString() + "                    " + read[1].ToString() + "\n";
+                //name += read[1].ToString() + "\n";
+            }
+
+
+            texty.Text = "Employee Id        Name\n" + data;
+            cn.Close();
+        }
+    
+    }
+}
